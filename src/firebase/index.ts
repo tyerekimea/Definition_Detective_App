96b1d752928c99a99e7d69a6fb6c0e04c36e745d
Tonbra Benjamin Yerekimea <yerekimeatonbaa@gmail.com>
2025-11-07 01:00:49 +0000

    i just published the app and noticed that user sign in is not working, h

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -7,29 +7,20 @@ import { getFirestore } from 'firebase/firestore';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (!getApps().length) {
-    // Important! initializeApp() is called without any arguments because Firebase App Hosting
-    // integrates with the initializeApp() function to provide the environment variables needed to
-    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
-    // without arguments.
-    let firebaseApp;
-    try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
-    } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
-
-    return getSdks(firebaseApp);
+  if (getApps().length) {
+    return getSdks(getApp());
   }
 
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
+  let firebaseApp;
+  // In a deployed Firebase App Hosting environment, the SDK is automatically configured.
+  // In a local environment, we use the firebaseConfig object.
+  if (process.env.NODE_ENV === 'production') {
+      firebaseApp = initializeApp();
+  } else {
+      firebaseApp = initializeApp(firebaseConfig);
+  }
+
+  return getSdks(firebaseApp);
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {

49fba3d7565ab0783b4edc32a69ecd28900a4e7d
Tonbra Benjamin Yerekimea <yerekimeatonbaa@gmail.com>
2025-11-12 20:29:43 +0000

    Error: Failed to fetch
    Call Stack
    2
    
    fetchServerAction
    file:///home/use

--- src/ai/flows/game-sounds-flow.ts
+++ src/ai/flows/game-sounds-flow.ts
@@ -11,6 +11,7 @@
 import {ai} from '@/ai/genkit';
 import {z} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import wav from 'wav';
 
 const GameSoundInputSchema = z
   .string()
@@ -30,6 +31,33 @@ export async function getGameSound(
   return gameSoundFlow(input);
 }
 
+async function toWav(
+  pcmData: Buffer,
+  channels = 1,
+  rate = 24000,
+  sampleWidth = 2
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const writer = new wav.Writer({
+      channels,
+      sampleRate: rate,
+      bitDepth: sampleWidth * 8,
+    });
+
+    let bufs = [] as any[];
+    writer.on('error', reject);
+    writer.on('data', function (d) {
+      bufs.push(d);
+    });
+    writer.on('end', function () {
+      resolve(Buffer.concat(bufs).toString('base64'));
+    });
+
+    writer.write(pcmData);
+    writer.end();
+  });
+}
+
 const gameSoundFlow = ai.defineFlow(
   {
     name: 'gameSoundFlow',
@@ -37,16 +65,29 @@ const gameSoundFlow = ai.defineFlow(
     outputSchema: GameSoundOutputSchema,
   },
   async query => {
-    const {media} = await ai.generate({
-      model: 'text-to-speech-1',
-      prompt: { tts: query },
+    const { media } = await ai.generate({
+      model: googleAI.model('gemini-2.5-flash-preview-tts'),
+      prompt: query,
+      config: {
+        responseModalities: ['AUDIO'],
+        speechConfig: {
+          voiceConfig: {
+            prebuiltVoiceConfig: { voiceName: 'Algenib' },
+          },
+        },
+      }
     });
     if (!media) {
       throw new Error('no media returned');
     }
     
+    const audioBuffer = Buffer.from(
+      media.url.substring(media.url.indexOf(',') + 1),
+      'base64'
+    );
+
     return {
-      soundDataUri: media.url,
+      soundDataUri: 'data:audio/wav;base64,' + (await toWav(audioBuffer)),
     };
   }
 );

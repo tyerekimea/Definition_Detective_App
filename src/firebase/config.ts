f2ed75fe8faebecc20fddb2fd70d5ced85e815c8
Tonbra Benjamin Yerekimea <yerekimeatonbaa@gmail.com>
2025-11-16 07:05:15 +0000

    Try fixing this error: `Console Error: [2025-11-16T07:02:49.745Z] @fireb

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,14 @@
+
 // This file reads the Firebase config from environment variables.
 // These variables are defined in the .env file and exposed to the client
 // in next.config.ts.
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "G-MEASUREMENT_ID",
 };

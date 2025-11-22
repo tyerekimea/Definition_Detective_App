25ff1fb9c23ff5bcec5eeec3d1a45c04b99aecf6
Tonbra Benjamin Yerekimea <yerekimeatonbaa@gmail.com>
2025-11-22 04:35:45 +0000

    the game has failed to load

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,5 +1,7 @@
-import { config } from 'dotenv'
-config()
 
-import './flows/game-sounds-flow'
-import './flows/generate-word-flow'
+import { config } from 'dotenv';
+config();
+
+import './flows/game-sounds-flow';
+import './flows/generate-word-flow';
+import './flows/generate-hints';

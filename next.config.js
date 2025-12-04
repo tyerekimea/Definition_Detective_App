cf2806f8fa8e2a67a141b8583f1ad3f9c0fed98f
Tonbra Benjamin Yerekimea <yerekimeatonbaa@gmail.com>
2025-12-04 12:43:54 +0000

    studio-4536174912:~/studio{main}$ npm run dev
    > definition-detective@0.

--- next.config.js
+++ next.config.js
@@ -1,45 +1,22 @@
-
 /** @type {import('next').NextConfig} */
-
-require('dotenv').config();
-
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
-  experimental: {
-    // No longer need allowedDevOrigins here
-  },
-  allowedDevOrigins: [
-      "http://localhost:9003",
-      "https://*.cloudworkstations.dev",
-      "https://*.firebase.studio",
-  ]
 };
 
 module.exports = nextConfig;

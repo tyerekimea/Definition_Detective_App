849577e586cbf262b95348c520e7c5ca9bb3c6ce
tyerekimea <tyerekimea@gmail.com>
2026-01-25 14:57:38 +0000

    Fix: Replace service worker with Monetag configuration
    Co-authored-by: Ona <no-reply@ona.com>

--- public/sw.js
+++ public/sw.js
@@ -1,34 +1,6 @@
-/* Simple service worker: cache assets on install and serve from cache */
-const CACHE_NAME = 'defdetective-v1';
-const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/favicon.ico',
-  '/manifest.json'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((k) => k !== CACHE_NAME)
-          .map((k) => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
-});
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 10514754
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
